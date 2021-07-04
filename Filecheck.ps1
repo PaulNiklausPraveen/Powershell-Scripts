6cc54ee293c84e069d6b49325740eb6fd60933e7
Paul Niklaus Praveen <62203157+PaulNiklausPraveen@users.noreply.github.com>
2021-07-04 11:07:52 +0530

    Add files via upload

---
+++ Filecheck.ps1
@@ -0,0 +1,18 @@
+﻿Set-Location E:\test
+try{
+$files = Get-ChildItem -Path "E:\test\test" -ErrorAction Stop
+ForEach($file in $files)
+{
+$f = Get-Content $file
+$f -contains $f[4]
+
+}
+
+}
+catch
+{
+$_.exception.message
+}
+
+
+
