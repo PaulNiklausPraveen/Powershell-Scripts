8746f3d224a917376354d506c334d52df7b0bb42
Paul Niklaus Praveen <62203157+PaulNiklausPraveen@users.noreply.github.com>
2023-03-24 11:27:21 +0530

    Enable RDP through CIM/WMI

--- Enable-RDP.ps1
+++ Enable-RDP.ps1
@@ -4,8 +4,8 @@
 $RDPSetting = Get-WmiObject -Class Win32_TerminalServiceSetting -Namespace Root\CimV2\TerminalServices -ComputerName REMOTESERVER
 $RDPSetting .SetAllowTSConnections(1,1)
 
-
 #For PowerShell Core(6,7) Versions run below script. As WMI is not supported in .Net core.
+#If you want enable locally, remove  -ComputerName paramerter  
 
 $RDPSetting = Get-CimInstance -Class Win32_TerminalServiceSetting -Namespace Root\CimV2\TerminalServices -ComputerName REMOTESERVER
 $RDPSetting .SetAllowTSConnections(1,1)
