0bf09854151b2bd1dc8829a6e4c4025bfcb852aa
Paul Niklaus Praveen <62203157+PaulNiklausPraveen@users.noreply.github.com>
2023-03-24 11:25:57 +0530

    Update Enable-RDP.ps1

--- Enable-RDP.ps1
+++ Enable-RDP.ps1
@@ -5,7 +5,7 @@ $RDPSetting = Get-WmiObject -Class Win32_TerminalServiceSetting -Namespace Root\
 $RDPSetting .SetAllowTSConnections(1,1)
 
 
-#For PowerShell Core(6,7 Versions run below script. As WMI is not supported in .Net core.
+#For PowerShell Core(6,7) Versions run below script. As WMI is not supported in .Net core.
 
 $RDPSetting = Get-CimInstance -Class Win32_TerminalServiceSetting -Namespace Root\CimV2\TerminalServices -ComputerName REMOTESERVER
 $RDPSetting .SetAllowTSConnections(1,1)
